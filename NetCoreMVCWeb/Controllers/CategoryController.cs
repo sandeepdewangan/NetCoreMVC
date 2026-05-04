a5cdcf097eb7bd83d657863d9a3c2089e1e486c4
sandeepdewangan <sandeepinwork@gmail.com>
2026-05-04 15:26:42 +0530

    Read Categories

--- NetCoreMVCWeb/Controllers/CategoryController.cs
+++ NetCoreMVCWeb/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreMVCWeb.Data;
 
 namespace NetCoreMVCWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+         _context = context;   
+        }
         public IActionResult Index()
         {
-            return View();
+            var categories = _context.Categories.ToList();
+            return View("Index", categories);
         }
     }
 }
