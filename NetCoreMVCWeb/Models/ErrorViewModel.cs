5093c4b9eff21e8bc9298eee6036f605c320b8f6
sandeepdewangan <sandeepinwork@gmail.com>
2026-05-04 12:17:48 +0530

    Add project files.

---
+++ NetCoreMVCWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCoreMVCWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
